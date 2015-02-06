f7c92e97160172ff32b7e8df77e1f2d0c695300f
Kat Carver <kat.yoshigirl@gmail.com>
2015-02-06 21:25:07 +0000

    a bit of movement

--- cnc.py
+++ cnc.py
@@ -4,12 +4,15 @@ pygame.init()
 screen = pygame.display.set_mode((800,600))
 background = pygame.image.load("background.bmp")
 bod = pygame.image.load("bod3.png")
+pygame.key.set_repeat(2,2)
 bodsize = 52
 backgroundRect = background.get_rect()
 pygame.display.flip()
 
 x = 400
 y = 300
+barheight = 125
+step = 5
 
 def draw():
     moved = 0;
@@ -17,24 +20,34 @@ def draw():
     screen.blit(bod, (x,y))
     pygame.display.flip()
 
+def tell():
+    print 'Bod is at (' + repr(x) + ',' + repr(y) + ')'
+
 draw()
 
 while 1:
     event = pygame.event.wait()
     if event.type == pygame.KEYDOWN:
+        tell()
         pressed = pygame.key.get_pressed()
         if pressed[pygame.K_UP]:
             print 'pressed UP'
-            y = y - bodsize
-            moved = 1
+            if (y > 0 + step):
+                y = y - step
+                moved = 1
         if pressed[pygame.K_DOWN]:
             print 'pressed DOWN'
-            y = y + bodsize
-            moved = 1
+            if (y < 600-barheight-step):
+               y = y + step
+               moved = 1
         if pressed[pygame.K_LEFT]:
             print 'pressed LEFT'
+            x = x - step
+            moved = 1
         if pressed[pygame.K_RIGHT]:
             print 'pressed RIGHT'
+            x = x + step
+            moved = 1
         if pressed[pygame.K_q]:
             print 'pressed Q'
             pygame.display.quit()
