5f8ae58bd4271fcf00b8271b5d1fb189acc17bea
Frank Carver <frank@efsol.com>
2015-02-06 09:56:57 +0000

    detect key presses

--- cnc.py
+++ cnc.py
@@ -3,7 +3,7 @@ pygame.init()
 
 screen = pygame.display.set_mode((640,480))
 
-background = pygame.image.load("balloons.jpg")
+background = pygame.image.load("bg.jpg")
 backgroundRect = background.get_rect()
 
 screen.blit(background, backgroundRect)
@@ -12,6 +12,17 @@ pygame.display.flip()
 while 1:
     event = pygame.event.wait()
     if event.type == pygame.KEYDOWN:
-        pygame.display.quit()
-        sys.exit()
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_UP]:
+            print 'pressed UP'
+        if pressed[pygame.K_DOWN]:
+            print 'pressed DOWN'
+        if pressed[pygame.K_LEFT]:
+            print 'pressed LEFT'
+        if pressed[pygame.K_RIGHT]:
+            print 'pressed RIGHT'
+        if pressed[pygame.K_q]:
+            print 'pressed Q'
+            pygame.display.quit()
+            sys.exit()
 
